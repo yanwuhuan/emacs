fa05c12e51e7b3dc441c8d5efd4b1f3ab76dcf87
Zhang Tanis <tanis.zhang@gmail.com>
2013-03-04 17:25:26 +0800

    Fix previous error

--- config/auto-complete-conf.el
+++ config/auto-complete-conf.el
@@ -1,14 +1,14 @@
 ;;; auto-complete-conf.el --- 
 ;; -*- coding: utf-8 -*-
 ;; -*- mode: Emacs-Lisp -*-
-;; Time-stamp: <2013-03-04 17:21:05 Tanis Zhang>
+;; Time-stamp: <2013-03-04 17:24:52 Tanis Zhang>
 ;; From http://emacser.com/auto-complete.htm
 
 
 ;; Add to load path
-(add-to-list 'load-path "/home/backfire/emacs/auto-complete")
+(add-to-list 'load-path "~/emacs/packages/auto-complete")
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "/home/backfire/emacs/auto-complete/ac-dict")
+(add-to-list 'ac-dictionary-directories "~/emacspackages/auto-complete/ac-dict")
 (ac-config-default)
 
 ;;; auto-complete-conf.el ends here

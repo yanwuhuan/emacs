71ce9052949e99e088aeb27c37e70943c67796e3
yanwuhuan <yanwuhuan@gmail.com>
2013-07-09 18:32:09 +0800

    Modify program path

--- config/lua-conf.el
+++ config/lua-conf.el
@@ -1,9 +1,10 @@
-;;; emacs.el ---
+;; lua-conf.el ---
 ;; -*- coding: utf-8 -*-
 ;; -*- mode: Emacs-Lisp -*-
-;; Time-stamp: <2012-07-06 14:46:17 Tanis Zhang>
+;; Time-stamp: <2013-07-09 18:29:35 Tanis Zhang>
 
 (add-to-list 'load-path "~/emacs/packages/lua-mode")
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+(setq lua-default-application "~/Apps/Develop/Lua/5.1/lua.exe")
